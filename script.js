45b509a272e495b1cc469171a0ec430387c5e9ea
Yogesh R <116285581+Yogesh73151915059@users.noreply.github.com>
2023-07-10 18:29:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here
 
+let bandNames = ['The Beatles', 'Aerosmith', 'Led Zeppelin', 'Pink Floyd', 'The Rolling Stones'];
+
+// Remove articles from band names
+let sortedBandNames = bandNames.map(name => name.replace(/^(a|an|the)\s+/i, ''));
+
+// Sort the band names in lexicographic order
+sortedBandNames.sort((a, b) => a.localeCompare(b));
+
+// Get the 'band' element by its ID
+let bandElement = document.getElementById('band');
+
+// Clear any existing content inside the 'band' element
+bandElement.innerHTML = '';
+
+// Add each band name as a list item
+sortedBandNames.forEach(name => {
+  let listItem = document.createElement('li');
+  listItem.textContent = name;
+  bandElement.appendChild(listItem);
+});
